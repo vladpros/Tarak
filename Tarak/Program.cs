e701b5cf702b8971dc9f7bb2fc39ae42ce92e658
vladpros <vlad_pros@mail.ru>
2019-01-24 11:50:08 +0200

    All

--- Tarak/Program.cs
+++ Tarak/Program.cs
@@ -2,14 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Tarak
 {
     class Program
     {
+        static int[] Mass = new int[4];
+        static object _locker = new object();
+
         static void Main(string[] args)
         {
+            var pts1 = new ParameterizedThreadStart(Threads);
+            TimerCallback tm = new TimerCallback(Monitor);
+
+
+            Timer timer = new Timer(tm, null, 0, 10000);
+            Thread.Sleep(1000);
+
+            List<Tarak> tarak = new List<Tarak>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                tarak.Add(new Tarak());
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Thread thread = new Thread(pts1);
+                tarak[i].i = i;
+                thread.Start(tarak[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void Threads(object obj1)
+        {
+
+            Tarak tar = (Tarak)obj1;
+
+            while (true)
+            {
+                Mass[tar.i] += tar.Move();
+
+                Thread.Sleep(3000);
+            }
+
+        }
+
+        static void Monitor(object obj)
+        {
+
+            Console.Clear();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Tarakan #{i} proshol {Mass[i]}m");
+            }
         }
     }
 }
---
+++ Tarak/Tarak.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarak
+{
+    class Tarak
+    {
+        Random _rand = new Random();
+
+        private int speed = 0;
+        public int i = 0;
+
+        public Tarak()
+        {
+            speed = _rand.Next(1,10);
+        }
+
+        public int Move()
+        {
+
+            return speed;
+        }
+    }
+}
